c5a07de2cee2a556c8c7bfd6ba7d800ee9babf7d
Himanshu1747 <himanshusaxena1747@gmail.com>
2026-01-19 17:59:29 +0530

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,32 @@
-// @ts-check
+// // @ts-check
 
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+// import { defineConfig } from 'astro/config';
+
+// import react from '@astrojs/react';
+// import node from '@astrojs/node';
+
+// import vercel from '@astrojs/vercel';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://example.com',
+//   integrations: [mdx(), sitemap(), react()],
+
+//   adapter: vercel(),
+// });
+
+
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless'; // This is the key change
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://greenmountainfencecompany.vercel.app',
   integrations: [mdx(), sitemap(), react()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server', // This allows your React components to handle dynamic logic
+  adapter: vercel(),
 });
